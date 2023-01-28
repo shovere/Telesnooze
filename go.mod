ddb180756613f13bcdf0601147996bc219fabeeb
shovere <58785391+shovere@users.noreply.github.com>
2023-01-27 20:22:56 -0500

    hello world

---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/m
+
+go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16
+)
---
+++ index.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	//"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+
+func setupDB() *sql.DB {
+	db, err := sql.Open("sqlite3","telesnooze.db");
+	fmt.Print(err);
+	return db;
+}
+
+func sayHello(writer http.ResponseWriter, request *http.Request){
+	fmt.Print("hello new user")
+	writer.Header().Set("Content-Type", "application/json")
+}
+
+func main(){
+	setupDB()
+	router := mux.NewRouter();
+	router.HandleFunc("/api/v1/", sayHello).Methods("GET");
+	fmt.Println("Server at 8123")
+    log.Fatal(http.ListenAndServe(":8123", router))
+}
