391794ee3a3d7aeb0ce789eaf78b3faf3dd4a6f7
shovere <58785391+shovere@users.noreply.github.com>
2023-02-07 00:02:11 -0500

    setting alarm works

--- Backend/index.go
+++ Backend/index.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"database/sql"
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 
-func setupDB() *sql.DB {
+
+type alarm struct {
+	Time string `json:"time"`
+	Week struct {
+		Sunday bool `json:"sunday"`
+		Monday bool `json:"monday"`
+		Tuesday bool `json:"tuesday"`
+		Wednesday bool `json:"wednesday"`
+		Thursday bool `json:"thursday"`
+		Friday bool `json:"friday"`
+		Saturday bool `json:"saturday"`
+	} `json:"days"`
+}
+
+type App struct {
+	router *mux.Router
+	DB *sql.DB
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+    respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
+}
+
+func (a *App) initializeApp() {
 	db, err := sql.Open("sqlite3","telesnooze.db");
-	fmt.Print(err);
-	return db;
+	if err != nil {
+		panic("failed to connect database")
+	  }
+	a.DB = db
+	a.router = mux.NewRouter();
 }
 
 func sayHello(writer http.ResponseWriter, request *http.Request){
@@ -23,10 +58,35 @@ func sayHello(writer http.ResponseWriter, request *http.Request){
 	writer.Header().Set("Content-Type", "application/json")
 }
 
+//this a *App  means it applies to the app struct type
+func (a *App) setAlarm(writer http.ResponseWriter, request *http.Request){
+	var alarm alarm
+	decoder := json.NewDecoder(request.Body);
+	fmt.Println(decoder)
+	id := uuid.New()
+	errDecode := decoder.Decode(&alarm);
+	fmt.Printf("%v: %v\n", alarm.Time, alarm.Week)
+	if (errDecode != nil) {
+		fmt.Println(errDecode)
+        respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+	_, err := a.DB.Exec(
+       `INSERT INTO alarms(id, time, sunday, monday, tuesday, wednesday, thursday,friday,saturday) 
+		VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`,
+        id, alarm.Time, alarm.Week.Sunday, alarm.Week.Monday, alarm.Week.Tuesday, alarm.Week.Wednesday, alarm.Week.Thursday, alarm.Week.Friday, alarm.Week.Saturday)
+	if(err != nil ){
+		fmt.Println(err)
+	}
+
+	defer request.Body.Close()
+}
+
 func main(){
-	setupDB()
-	router := mux.NewRouter();
-	router.HandleFunc("/api/v1/", sayHello).Methods("GET");
+	app := &App{}
+	app.initializeApp()
+	app.router.HandleFunc("/api/v1/", sayHello).Methods("GET");
+	app.router.HandleFunc("/api/v1/setAlarm", app.setAlarm).Methods("POST")
 	fmt.Println("Server at 8123")
-    log.Fatal(http.ListenAndServe(":8123", router))
+    log.Fatal(http.ListenAndServe(":8123",app.router))
 }
