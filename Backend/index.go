fb2cf73d7db4f6a1aa9a296829cbb7d992d86b2d
Sean Hernandez <97011969+NotMagolor@users.noreply.github.com>
2023-02-07 22:55:34 -0500

    Update index.go

--- Backend/index.go
+++ Backend/index.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"crypto/sha256"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,7 +14,12 @@ import (
 )
 
 
-
+type account struct {
+	Email string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Phone string `json:"phone"`
+}
 type alarm struct {
 	Time string `json:"time"`
 	Week struct {
@@ -30,6 +36,34 @@ type alarm struct {
 type App struct {
 	router *mux.Router
 	DB *sql.DB
+}
+
+func hashPassword(password string) []byte {
+	h := sha256.New()
+	h.Write([]byte(password))
+	return h.Sum(nil)
+}
+
+func (a *App) createUser(writer http.ResponseWriter, request *http.Request) {
+	var account account
+	decoder := json.NewDecoder(request.Body);
+    errDecode := decoder.Decode(&account);
+	id := uuid.New()
+	if (errDecode != nil) {
+		fmt.Println(errDecode)
+        respondWithError(writer, http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+	hashedPassword := hashPassword(account.Password)
+	_, err := a.DB.Exec("INSERT INTO users(id, email, username, password, phone) VALUES($1, $2, $3, $4, $5) RETURNING id",
+							id, account.Email, account.Username, hashedPassword, account.Phone)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer request.Body.Close()
+
 }
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
@@ -87,6 +121,7 @@ func main(){
 	app.initializeApp()
 	app.router.HandleFunc("/api/v1/", sayHello).Methods("GET");
 	app.router.HandleFunc("/api/v1/setAlarm", app.setAlarm).Methods("POST")
+	app.router.HandleFunc("/api/v1/createUser", app.createUser).Methods("POST")
 	fmt.Println("Server at 8123")
     log.Fatal(http.ListenAndServe(":8123",app.router))
 }
