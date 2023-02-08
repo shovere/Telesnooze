6cd608eb7f05474968e8ceb65d243ac0eb2a9590
Jesse Maki <87655161+jessexmaki@users.noreply.github.com>
2023-02-07 21:12:11 -0500

    Add files via upload

--- Frontend/app/app.module.ts
+++ Frontend/app/app.module.ts
@@ -1,46 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TitleComponent } from './title/title.component';
+import { MatButton } from '@angular/material/button';
+import {MaterialExampleModule} from '../material.module';
+import {activealarmgroup} from './alarm';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    TitleComponent
-  ],
+  declarations: [activealarmgroup],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    BrowserAnimationsModule,
-    BrowserAnimationsModule,
     FormsModule,
-    MatToolbarModule,
-    MatInputModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTableModule,
-    MatSlideToggleModule,
-    MatSelectModule,
-    MatOptionModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    NgxMaterialTimepickerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [activealarmgroup],
 })
-export class AppModule { }
+export class AppModule {}
