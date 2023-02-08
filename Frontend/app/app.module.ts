8868504e1b97f95d395a149665d61244861f07b0
perezandy <97133417+perezandy@users.noreply.github.com>
2023-02-07 22:17:46 -0500

    Add files via upload
    Full integration with time picker

--- Frontend/app/app.module.ts
+++ Frontend/app/app.module.ts
@@ -1,29 +1,67 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { MatButton } from '@angular/material/button';
-import {MaterialExampleModule} from '../material.module';
-import {activealarmgroup} from './alarm';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
-import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TitleComponent } from './title/title.component';
+import { SignupComponent } from './signup/signup.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxMaterialTimepickerModule } from "ngx-material-timepicker";
+import { ReactiveFormsModule } from '@angular/forms';
 import { AlarmComponent } from './alarm/alarm.component';
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @NgModule({
-  declarations: [activealarmgroup, AlarmComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    TitleComponent,
+    SignupComponent,
+    HomeComponent,
+    AlarmComponent
+  ],
   imports: [
-    BrowserAnimationsModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatOptionModule,
+    AppRoutingModule,
+    MatProgressBarModule,
     FormsModule,
     HttpClientModule,
     MatNativeDateModule,
-    MaterialExampleModule,
     ReactiveFormsModule,
     NgxMaterialTimepickerModule,
+    MatCheckboxModule
   ],
   providers: [],
-  bootstrap: [activealarmgroup],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
